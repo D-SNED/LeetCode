de4da67e9807a9d470168fbafc28b5c509343325
D-SNED <133810797+D-SNED@users.noreply.github.com>
2024-01-26 14:35:23 -0800

    Time: 721 ms (99.10%), Space: 17.2 MB (53.23%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -6,20 +6,32 @@ class Solution:
         # iterate over each row
             # append sum(row) to row_totals list
             
+        num_ones = 0
+        index = 0
+            
+        for iteration, row in enumerate(mat):
+            count = row.count(1)
+            
+            if count > num_ones:
+                num_ones = count
+                index = iteration
+                
+        return [index, num_ones]
+            
         
         
-        row_totals = []
+#         row_totals = []
        
         
-        for i in range(len(mat)):
-            row_totals.append(sum(mat[i]))
+#         for i in range(len(mat)):
+#             row_totals.append(sum(mat[i]))
         
-        _max = max(row_totals)
+#         _max = max(row_totals)
             
-        for i in range(len(row_totals)):    
-            if len(row_totals) > 0 and row_totals[i] == _max:
-                return [i, _max]
-        return [0, 0]
+#         for i in range(len(row_totals)):    
+#             if len(row_totals) > 0 and row_totals[i] == _max:
+#                 return [i, _max]
+#         return [0, 0]
         
         
         
