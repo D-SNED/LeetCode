6eda45283ba1d46277aa28703c986eceee679e02
D-SNED <133810797+D-SNED@users.noreply.github.com>
2024-02-05 09:21:49 -0800

    Time: 314 ms (32.07%), Space: 16.5 MB (61.46%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,7 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 2:
-            return n
+#         if n < 2:
+#             return n
         
-        while n >= 2:
-            return self.fib(n-1) + self.fib(n - 2)
+#         while n >= 2:
+#             return self.fib(n-1) + self.fib(n - 2)
+
+        if n <= 1:
+            return n
+        else:
+            return self.fib(n - 1) + self.fib(n - 2)
