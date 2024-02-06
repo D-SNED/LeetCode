f0b43eae219a55d954d77a6fbe29b75503ae891e
D-SNED <133810797+D-SNED@users.noreply.github.com>
2024-02-05 18:42:36 -0800

    Time: 58 ms (78.56%), Space: 17.7 MB (66.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,13 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums) - 1):
-            for j in range(1,len(nums)):
-                if nums[i] + nums[j] == target and j != i:
-                    return [i,j]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        
+        for index, num in enumerate(nums):
+            new_targ = target - num
+            
+            if new_targ in dict:
+                return [dict[new_targ], index]
+            dict[num] = index
+                
+        
         
