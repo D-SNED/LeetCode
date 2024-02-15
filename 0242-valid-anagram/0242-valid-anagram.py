f5b4d79a402efa9e5a1508461176f9c154f85b31
D-SNED <133810797+D-SNED@users.noreply.github.com>
2024-02-15 09:21:13 -0800

    Time: 52 ms (50.71%), Space: 16.9 MB (88.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-#         sd = {}
+        sd = {}
         
-#         td = {}
+        td = {}
         
-#         for char in s:
-#             if char not in sd:
-#                 sd[char] = 0
-#             sd[char] += 1
+        for char in s:
+            if char not in sd:
+                sd[char] = 0
+            sd[char] += 1
             
-#         for char in t:
-#             if char not in td:
-#                 td[char] = 0
-#             td[char] += 1
+        for char in t:
+            if char not in td:
+                td[char] = 0
+            td[char] += 1
             
-#         return sd == td
+        return sd == td
         
-        s_sorted = sorted(s)
-        t_sorted = sorted(t)
+#         s_sorted = sorted(s)
+#         t_sorted = sorted(t)
         
-        return s_sorted == t_sorted
+#         return s_sorted == t_sorted
         
