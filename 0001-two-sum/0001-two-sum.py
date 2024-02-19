910bf868bc547f4706b02dbac51126f64b69e9da
D-SNED <133810797+D-SNED@users.noreply.github.com>
2024-02-19 10:40:26 -0800

    Time: 48 ms (97.41%), Space: 17.8 MB (70.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        index = 0
+#         index = 0
                    
         
-        while index < len(nums):
-            curr = nums[index] 
-            comp = target - curr
-            print(comp, curr)
-            for i in range(index + 1, len(nums)):
-                if nums[i] == comp:
-                    return [index, i]
-            index += 1
-        return []
+#         while index < len(nums):
+#             curr = nums[index] 
+#             comp = target - curr
+#             print(comp, curr)
+#             for i in range(index + 1, len(nums)):
+#                 if nums[i] == comp:
+#                     return [index, i]
+#             index += 1
+#         return []
             
+        d = {}
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
             
+            if diff in d:
+                return [d[diff], i]
+            d[nums[i]] = i
                 
         
         
