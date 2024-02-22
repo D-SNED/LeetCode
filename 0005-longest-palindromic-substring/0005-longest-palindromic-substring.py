9bd9d1eaafecacf1739ded2d6747573b1ed680d2
D-SNED <133810797+D-SNED@users.noreply.github.com>
2024-02-22 11:08:35 -0800

    Time: 8167 ms (7.27%), Space: 16.5 MB (91.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -8,24 +8,34 @@ class Solution:
                     # append into result
                     
         
-        substring = []
-        for i in range(len(s)):
+#         substring = []
+#         for i in range(len(s)):
             
-            copy = s[i::]
-            # print("first: ",copy)
-            length = len(copy)
-            while length > 0:
-                if copy == copy[::-1]:
-                    substring.append(copy)
-                copy = copy[:len(copy) - 1]
-                length -= 1
-                # print("while: ", copy)
+#             copy = s[i::]
+#             # print("first: ",copy)
+#             length = len(copy)
+#             while length > 0:
+#                 if copy == copy[::-1]:
+#                     substring.append(copy)
+#                 copy = copy[:len(copy) - 1]
+#                 length -= 1
+#                 # print("while: ", copy)
         
-        max = ""
-        for string in substring:
-            if len(string) > len(max):
-                max = string
-        return max
+#         max = ""
+#         for string in substring:
+#             if len(string) > len(max):
+#                 max = string
+#         return max
+
+        result = ""
+    
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                substring = s[i:j]
+                if substring == substring[::-1] and len(substring) > len(result):
+                    result = substring
+
+        return result
                 
         
             
