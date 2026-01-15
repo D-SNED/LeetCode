0da569119c293e163dad9fe95e6a2f48d22cf4d2
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-01-15 09:27:51 -0800

    Time: 1 ms (54.61%), Space: 23.7 MB (6.95%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,13 +3,15 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        l = 0 
-        r = len(s) - 1
+        stack = []
 
-        while l < r:
-            s[l] , s[r] = s[r], s[l]
-            l += 1
-            r -= 1
+        for char in s:
+            stack.append(char)
+
+        i = 0
+        while stack:
+            s[i] = stack.pop()
+            i += 1
 
 
 
