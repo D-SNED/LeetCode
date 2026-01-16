c622384e0b94009407bfe74c3221d83fa15075cd
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-01-16 07:43:07 -0800

    Time: 11 ms (30.5%), Space: 21.2 MB (9.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,20 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d = {}
-        
+        d = Counter(nums)
+
         for num in nums:
-            if num not in d:
-                d[num] = 0
-            d[num] += 1
-            
-        maxi = 0
-        
-        for key in d:
-            if d[key] > maxi:
-                maxi = d[key]
-                
-        for key in d:
-            if d[key] == maxi:
-                return key
-                
-        
+            if d[num] > len(nums) // 2:
+                return num
