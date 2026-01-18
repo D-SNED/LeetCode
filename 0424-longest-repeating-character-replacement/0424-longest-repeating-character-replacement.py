80fbce7063e6ad6452ced059fbce058975829bb4
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-01-18 08:24:08 -0800

    Time: 79 ms (93.29%), Space: 19.7 MB (18.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,10 +1,10 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         res = 0
-        l = 0
-        max_f = 0
 
         d = {}
+        l = 0
+        max_f = 0
 
         for r in range(len(s)):
             d[s[r]] = 1 + d.get(s[r], 0)
@@ -13,7 +13,6 @@ class Solution:
             while (r - l + 1) - max_f > k:
                 d[s[l]] -= 1
                 l += 1
-
             res = max(res, r - l + 1)
 
         return res
