72161a28e34f55e195de2d64a660299bbd916c72
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-01-20 21:30:11 -0800

    Time: 187 ms (76.84%), Space: 25.9 MB (15.59%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -7,13 +7,14 @@ class Solution:
         l = 0
 
         for r in range(len(fruits)):
-            d[fruits[r]] = 1 + d.get(fruits[r], 0)
-            if len(d) > 2:
+            d[fruits[r]] = 1 + d.get(fruits[r],0)
+
+            while len(d) > 2:
                 d[fruits[l]] -= 1
                 if d[fruits[l]] == 0:
                     d.pop(fruits[l])
                 l += 1
-            
+
             res = max(res, r - l + 1)
 
         return res
