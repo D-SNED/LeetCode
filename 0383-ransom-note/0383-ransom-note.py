d12d8f84ff8b7496391f3a6981431f4bd32d06eb
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-01-20 21:17:05 -0800

    Time: 13 ms (82.71%), Space: 19.8 MB (9.48%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -5,9 +5,10 @@ class Solution:
         for char in ransomNote:
             if char not in count_m:
                 return False
-            elif char in count_m and count_m[char] == 0:
-                return False
+
             count_m[char] -= 1
 
+        for num in count_m.values():
+            if num < 0:
+                return False
         return True
-            
