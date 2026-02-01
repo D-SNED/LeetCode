3ff86bc8755cdca04489d01051d2acee3ab40c1e
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-02-01 07:47:10 -0800

    Time: 154 ms (89.24%), Space: 25.2 MB (42.79%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -2,17 +2,18 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         res = 0
         d = {}
+
         l = 0
 
         for r in range(len(fruits)):
-            d[fruits[r]] = 1 + d.get(fruits[r] , 0)
+            d[fruits[r]] = 1 + d.get(fruits[r], 0)
 
             if len(d) > 2:
                 d[fruits[l]] -= 1
-                if d[fruits[l]] == 0:
+                if d[fruits[l]]  == 0:
                     d.pop(fruits[l])
                 l += 1
-            
+
             res = max(res, r - l + 1)
 
         return res
