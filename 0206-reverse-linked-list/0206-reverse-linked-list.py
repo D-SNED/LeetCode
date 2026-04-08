bdde3897b1291be53b33151718744c7fe2e4d3ad
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-04-08 16:42:23 -0700

    Time: 0 ms (100%), Space: 20.6 MB (41.93%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -7,7 +7,7 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
         curr = head
-
+        
         while curr:
             temp = curr.next
             curr.next = prev
@@ -15,4 +15,4 @@ class Solution:
             curr = temp
 
         return prev
-        
+            
