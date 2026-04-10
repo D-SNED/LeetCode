661bb78714ff9e7f889b4b3fe3958dfed32ab2c0
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-04-09 19:32:10 -0700

    Time: 0 ms (100%), Space: 20.3 MB (92.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,14 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         l = 0
-        r = 1
-
-        while r < len(nums):
-            if nums[l] == 0 and nums[r] != 0:
-                nums[l] , nums[r] = nums[r] , nums[l]
+        
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
                 l += 1
-            elif nums[l] != 0:
-                l += 1
-            r += 1
-            
-            print(r)
