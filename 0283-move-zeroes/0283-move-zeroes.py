4cb721e0621a1bb99909935e17344fe7b3e1562b
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-04-09 19:22:12 -0700

    Time: 23 ms (10%), Space: 20.6 MB (37.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,10 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        last_non_zero_index = 0
+        l = 0
+        r = 1
 
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[last_non_zero_index] = nums[last_non_zero_index], nums[i]
-                last_non_zero_index += 1
-        
+        while r < len(nums):
+            if nums[l] == 0 and nums[r] != 0:
+                nums[l] , nums[r] = nums[r] , nums[l]
+                l += 1
+            elif nums[l] != 0:
+                l += 1
+            r += 1
+            
+            print(r)
