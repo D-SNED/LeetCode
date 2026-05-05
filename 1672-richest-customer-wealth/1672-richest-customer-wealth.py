75a07a034c3ff10bed466138f97701a477db5830
D-SNED <133810797+D-SNED@users.noreply.github.com>
2026-05-04 23:25:49 -0700

    Time: 0 ms (100%), Space: 19.2 MB (81.9%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,3 +1,11 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        return max([sum(acc) for acc in accounts])
+        max_wealth = 0
+
+        for account in accounts:
+            account_sum = sum(account)
+
+            if account_sum > max_wealth:
+                max_wealth = account_sum
+
+        return max_wealth
